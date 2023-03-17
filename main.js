a003ca0bf18f497d5812691e63ff1f3fe3af922f
Dario <darionas@gmail.com>
2023-03-17 21:18:22 +0200

    update index.html_5, main.css_3, add main.js, delete fetch_test.html

---
+++ main.js
@@ -0,0 +1,48 @@
+'Use strict';
+
+let evaluation = document.querySelector('.evaluation');
+let title, categories;
+
+
+getJSON('data.json');
+
+    async function getJSON(file) {
+        let myObject = await fetch(file);
+        let myData = await myObject.text();
+        let get = JSON.parse(myData);
+      
+        let i;
+        for(i=0; i < get.length; i++) {
+            let results = document.createElement('div');
+            evaluation.appendChild(results);
+            results.innerHTML = 
+            `<div class="evaluation-container">
+                <img class="evaluation-container__img" src="${get[i].icon}" alt="${get[i].category}" />
+                <div class="evaluation-container__title">${get[i].category}</div>
+                <div class="evaluation-container__value"><span class="score">${get[i].score}</span><span class="from"> / 100</span></div>
+            </div>`;
+            title = document.getElementsByClassName('evaluation-container__title');        
+            categories = '';
+            for(let a=0; a < title.length; a++) {
+                categories = title[a];
+                let evaluation_container =  document.getElementsByClassName('evaluation-container')[a];
+                let evaluation_containerTitle = document.getElementsByClassName('evaluation-container__title')[a];
+                if(categories.innerHTML == 'Reaction') {
+                    evaluation_container.classList.add('reaction-bgCol');
+                    evaluation_containerTitle.classList.add('reaction-col');
+                }
+                if(categories.innerHTML == 'Memory') {
+                    evaluation_container.classList.add('memory-bgCol');
+                    evaluation_containerTitle.classList.add('memory-col');
+                }
+                if(categories.innerHTML == 'Verbal') {
+                    evaluation_container.classList.add('verbal-bgCol');
+                    evaluation_containerTitle.classList.add('verbal-col');
+                }
+                if(categories.innerHTML == 'Visual') {
+                    evaluation_container.classList.add('visual-bgCol');
+                    evaluation_containerTitle.classList.add('visual-col');
+                }
+            }
+       }
+    }
