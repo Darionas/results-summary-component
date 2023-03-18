84689bdbbcb47832099983cb0bcb4af9922a343d
Dario <darionas@gmail.com>
2023-03-18 22:49:41 +0200

    update index.html_6, main.css_4, main.js_2, README.md_5

--- README.md
+++ README.md
@@ -1,27 +1,81 @@
-# Frontend Mentor challenge - Results summary component
+# Frontend Mentor - Results summary component solution
 
-## Welcome! 👋
+This is a solution to the [Results summary component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV).
 
-## Challenge based on
+## Table of contents
 
-- HTML
-- CSS
-- JavaScript
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
 
-## Main goal for challenge
+## Overview
+
+### The challenge
+
+Users should be able to:
 
 - View the optimal layout for the interface depending on their device's screen size
 - See hover and focus states for all interactive elements on the page
-- Implement JSON data
+- Fetch data from data.json file
 
-## Plan workflow
+### Screenshot
 
-- [x] Create drawing from design (structure of design)(1st task)
-- [x] Create of repo (2nd task)
-- [x] Create HTML layout (3rd task)
-- [x] Create CSS layout according style guides (4th task)
-- [x] Create grid layout for mobile first (5th task)
-- [ ] Craete grid layout for desktop (6th task)
-- [x] Implement JavaScript (partly)(7th task)
-- [ ] Overview of solution and RWD (8th task)
-- [ ] Tada it is done. Congratulation you reach your goal.🎉
+![Result_summary_component](./assets/images/result-summary-component.png)
+
+
+### Links
+
+- Solution URL: [Result summary component](https://github.com/Darionas/results-summary-component)
+- Live Site URL: [Result summary component](https://darionas.github.io/results-summary-component/)
+
+## My process
+
+ * Create drawing from design (structure of design)
+ * Create of repo
+ * Create HTML layout
+ * Create CSS layout according style guides
+ * Create grid layout for mobile first
+ * Craete grid layout for desktop
+ * Implement JavaScript:
+    * fetch data from data.json file
+
+### Built with
+
+- HTML
+- CSS
+- JavaCsript
+
+But created with love
+
+### What I learned
+
+- Digging deeper into data fetching
+
+### Continued development
+
+- Grid layout module.
+- JavaScript (fetching data).
+- Practise dealing with Git & GitHub.
+
+### Useful resources
+
+- [Fetch data_1](https://www.javascripttutorial.net/javascript-fetch-api/) - This guides introduces data fetching technique.  
+- [Fetch data_2](https://www.w3schools.com/js/js_api_fetch.asp) - This guides introduces data fetching technique. 
+
+
+## Author
+
+- Frontend Mentor - [@Darionas](https://www.frontendmentor.io/profile/Darionas)
+
+## Acknowledgments
+
+- Thank you Frontend Mentor team for opportunity to try, practice, train yourself in different level challenges and gain invaluable experience.
+- Thank you @Vikram from Frontend Mentor for guidance.
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 'Use strict';
+/*jshint esversion: 8*/
 
 let evaluation = document.querySelector('.evaluation');
 let title, categories;
@@ -7,42 +8,53 @@ let title, categories;
 getJSON('data.json');
 
     async function getJSON(file) {
-        let myObject = await fetch(file);
-        let myData = await myObject.text();
-        let get = JSON.parse(myData);
+         let myObject = await fetch(file);
+        try {
+            let myData = await myObject.text();
+            let get = JSON.parse(myData);
       
-        let i;
-        for(i=0; i < get.length; i++) {
-            let results = document.createElement('div');
-            evaluation.appendChild(results);
-            results.innerHTML = 
-            `<div class="evaluation-container">
-                <img class="evaluation-container__img" src="${get[i].icon}" alt="${get[i].category}" />
-                <div class="evaluation-container__title">${get[i].category}</div>
-                <div class="evaluation-container__value"><span class="score">${get[i].score}</span><span class="from"> / 100</span></div>
-            </div>`;
-            title = document.getElementsByClassName('evaluation-container__title');        
-            categories = '';
-            for(let a=0; a < title.length; a++) {
-                categories = title[a];
-                let evaluation_container =  document.getElementsByClassName('evaluation-container')[a];
-                let evaluation_containerTitle = document.getElementsByClassName('evaluation-container__title')[a];
-                if(categories.innerHTML == 'Reaction') {
-                    evaluation_container.classList.add('reaction-bgCol');
-                    evaluation_containerTitle.classList.add('reaction-col');
-                }
-                if(categories.innerHTML == 'Memory') {
-                    evaluation_container.classList.add('memory-bgCol');
-                    evaluation_containerTitle.classList.add('memory-col');
-                }
-                if(categories.innerHTML == 'Verbal') {
-                    evaluation_container.classList.add('verbal-bgCol');
-                    evaluation_containerTitle.classList.add('verbal-col');
-                }
-                if(categories.innerHTML == 'Visual') {
-                    evaluation_container.classList.add('visual-bgCol');
-                    evaluation_containerTitle.classList.add('visual-col');
+            for(let i=0; i < get.length; i++) {
+                let results = document.createElement('div');
+                evaluation.appendChild(results);
+                results.innerHTML = 
+                    `<div class="evaluation-container">
+                         <img class="evaluation-container__img" src="${get[i].icon}" alt="${get[i].category}" />
+                         <div class="evaluation-container__title">${get[i].category}</div>
+                         <div class="evaluation-container__value"><span class="score">${get[i].score}</span><span class="from"> / 100</span></div>
+                    </div>`;
+                title = document.getElementsByClassName('evaluation-container__title');        
+                categories = '';
+                for(let a=0; a < title.length; a++) {
+                    categories = title[a];
+                    let evaluation_container =  document.getElementsByClassName('evaluation-container')[a];
+                    let evaluation_containerTitle = document.getElementsByClassName('evaluation-container__title')[a];
+                    if(categories.innerHTML == 'Reaction') {
+                        evaluation_container.classList.add('reaction-bgCol');
+                        evaluation_containerTitle.classList.add('reaction-col');
+                    }
+                    if(categories.innerHTML == 'Memory') {
+                        evaluation_container.classList.add('memory-bgCol');
+                        evaluation_containerTitle.classList.add('memory-col');
+                    }
+                    if(categories.innerHTML == 'Verbal') {
+                        evaluation_container.classList.add('verbal-bgCol');
+                        evaluation_containerTitle.classList.add('verbal-col');
+                    }
+                    if(categories.innerHTML == 'Visual') {
+                        evaluation_container.classList.add('visual-bgCol');
+                        evaluation_containerTitle.classList.add('visual-col');
+                   }
                 }
             }
+            let button = document.createElement('div');
+            //button.classList.add('btn-container');
+            evaluation.appendChild(button);
+                button.innerHTML = 
+                    `<div class="btn-container">
+                        <button class="btn">Continue</button>
+                    </div>`;
+
+        } catch (error) {
+            console.log(error);
        }
     }
